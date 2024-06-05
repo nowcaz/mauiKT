b730d6c1fa64510e1dfaaeb73f776826f18d405c
Opilane TTHK <opilane@tthk.ee>
2024-06-05 11:47:25 +0300

    Valmis

--- Maui-main/Maui-main/Maui/obj/Debug/net7.0-ios/iossimulator-x64/Maui.AssemblyInfo.cs
+++ Maui-main/Maui-main/Maui/obj/Debug/net7.0-ios/iossimulator-x64/Maui.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0c4c5e15d73b57abc13f1927bf5f481a1e696c01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
